e6910528c6e3d509b6a104b171156b6220c83a1e
ramity <lcb005@uark.edu>
2016-10-24 22:12:19 -0500

    movement update - issue of character not being able to touch walls because math is hard

--- index.js
+++ index.js
@@ -1,7 +1,91 @@
 (function(){
+  //write('Hi','./engine/data/1.json');
+  //open('./engine/data/1.json');
   initDisplay();
+  initMovement();
   setInterval(main,20);
 })();
+
+function initMovement()
+{
+  const kd = require('keydrown');
+
+  kd.run(function () {
+    kd.tick();
+  });
+
+  kd.W.down(function(){testMove(character.x,character.y-character.speed.y)});
+  kd.A.down(function(){testMove(character.x-character.speed.x,character.y)});
+  kd.S.down(function(){testMove(character.x,character.y+character.speed.y)});
+  kd.D.down(function(){testMove(character.x+character.speed.x,character.y)});
+}
+
+function testMove(x,y)
+{
+  clear = true;
+
+  for(i in window.map)
+  {
+    if(between(x,window.map[i].x,window.map[i].x+window.tileSize.w))
+    {
+      if(between(y,window.map[i].y,window.map[i].y+window.tileSize.h))
+      {
+        clear = false;
+      }
+    }
+    if(between(x+window.tileSize.w,window.map[i].x,window.map[i].x+window.tileSize.w))
+    {
+      if(between(y,window.map[i].y,window.map[i].y+window.tileSize.h))
+      {
+        clear = false;
+      }
+    }
+    if(between(x,window.map[i].x,window.map[i].x+window.tileSize.w))
+    {
+      if(between(y+window.tileSize.h,window.map[i].y,window.map[i].y+window.tileSize.h))
+      {
+        clear = false;
+      }
+    }
+    if(between(x+window.tileSize.w,window.map[i].x,window.map[i].x+window.tileSize.w))
+    {
+      if(between(y+window.tileSize.h,window.map[i].y,window.map[i].y+window.tileSize.h))
+      {
+        clear = false;
+      }
+    }
+  }
+
+  if(clear)
+  {
+    character.x = x;
+    character.y = y;
+    console.log("T " + x,y);
+  }
+  else
+  {
+    console.log("F " + x,y);
+  }
+}
+
+function between(value,min,max)
+{
+  if(min <= value)
+  {
+    if(value <= max)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  else
+  {
+    return false;
+  }
+}
 
 function initDisplay()
 {
@@ -22,11 +106,17 @@ function initDisplay()
     h : canvas.height / tileCount.h
   };
 
+  window.map = [{x:-2*window.tileSize.w,y:-2*window.tileSize.h}];
+
   window.character = {
     display_x : (window.canvas.width / 2) - (window.tileSize.w / 2) + window.offset,
     display_y : (window.canvas.height / 2) - (window.tileSize.h / 2) + window.offset,
     x : 0,
-    y : 0
+    y : 0,
+    speed : {
+      x : window.tileSize.w / 20,
+      y : window.tileSize.h / 20
+    },
   };
 }
 
@@ -45,9 +135,50 @@ function render()
   {
     for(x=0;x<window.tileCount.w;x++)
     {
-      xpos = (x * window.tileSize.w) + offset;
-      ypos = (y * window.tileSize.h) + offset;
+      xpos = -window.character.x + (x * window.tileSize.w) + offset;
+      ypos = -window.character.y + (y * window.tileSize.h) + offset;
       ctx.strokeRect(xpos,ypos,window.tileSize.w,window.tileSize.h);
     }
   }
+
+  for(i in window.map)
+  {
+    ctx.fillStyle="#F00";
+    mapx = -window.character.x + window.character.display_x + window.map[i].x;
+    mapy = -window.character.y + window.character.display_y + window.map[i].y;
+    ctx.fillRect(mapx,mapy,window.tileSize.w,window.tileSize.h);
+    ctx.fillStyle="#000";
+  }
+}
+
+function write(input,filepath)
+{
+  var fs = require('fs');
+  fs.writeFile(filepath,input,function(err)
+  {
+    if(err)
+    {
+      return console.log(err);
+    }
+    else
+    {
+      console.log("The file was saved!");
+    }
+  });
+}
+
+function open(filepath)
+{
+  var fs = require('fs');
+  fs.readFile(filepath,'utf8',function(err,data)
+  {
+    if(err)
+    {
+      console.log(err);
+    }
+    else
+    {
+      return data;
+    }
+  });
 }
